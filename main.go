545f0d5a12b58af8dba1f70a580bb46cbc633a7b
Helmi Zulfikar <helmi.zulfikar20@gmail.com>
2018-10-10 13:21:57 +0700

    fix delete guru

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 
 	originsOk := handlers.AllowedOrigins([]string{"*"})
 	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Accept", "Authorization"})
-	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "OPTIONS", "DELETE", "PUT"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "DELETE", "PUT", "POST", "HEAD", "OPTIONS"})
 
 	if err := http.ListenAndServe(":7000", handlers.CORS(originsOk, headersOk, methodsOk)(r)); err != nil {
 		log.Fatal(err)
@@ -41,5 +41,5 @@ func guru(r *mux.Router) {
 	r.HandleFunc("/guru/{id}", ctr.CariGuru).Methods("GET")
 	r.HandleFunc("/guru", ctr.DaftarGuru).Methods("POST")
 	r.HandleFunc("/guru/{id}", ctr.UpdateGuru).Methods("PUT")
-	r.HandleFunc("/guru", ctr.DeleteGuru).Methods("DELETE")
+	r.HandleFunc("/guru/{id}", ctr.DeleteGuru).Methods("DELETE")
 }
